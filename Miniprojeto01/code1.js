95264742238d6c555d4bc776bf8da9e2f75f59ab
unknown <tamaragleicee56@gmail.com>
2022-08-01 18:48:58 -0300

    Trocando Valores de Variáveis

--- Miniprojeto01/code1.js
+++ Miniprojeto01/code1.js
@@ -1,7 +1,7 @@
-// let idade = 22;
-// console.log(idade);
-// let altura = 156;
-// console.log(altura);
+let idade = 22;
+console.log(idade);
+let altura = 156;
+console.log(altura);
 
 let Nome = 'Tamara';
 console.log(Nome);
