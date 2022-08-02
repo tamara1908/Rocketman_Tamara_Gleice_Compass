35728a6cec598db9c515a0494e368cf24cbb2f89
unknown <tamaragleicee56@gmail.com>
2022-08-02 12:04:33 -0300

    FizzBuzz

--- Miniprojeto03/code3.js
+++ Miniprojeto03/code3.js
@@ -3,7 +3,7 @@
 //Divisível por 3 e 5 => FizzBuzz
 //Não divisível por 3 ou 5 => entrada
 //Não é um número => 'Não é um número'
-const resultado = fizzBuzz(1);
+const resultado = fizzBuzz(25);
 console.log(resultado);
 
 function fizzBuzz(entrada){
