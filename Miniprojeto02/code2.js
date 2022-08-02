9756b11ad33cf90db170f2f94ffebb529b5591cf
unknown <tamaragleicee56@gmail.com>
2022-08-02 10:33:55 -0300

    Máximo Entre Dois Valores

---
+++ Miniprojeto02/code2.js
@@ -0,0 +1,15 @@
+//let valorMaior = max(19,19);
+//console.log(valorMaior);
+
+//function max (numero1,numero2) {
+//    if(numero1 > numero2)
+ //   return numero1;
+//} Uma outra maneira de retorno só que mais limpa;
+
+let valorMaior = max(13,20);
+console.log(valorMaior);
+
+function max (numero1,numero2) {
+    return numero1>numero2 ? numero1 : numero2 ;
+}
+
