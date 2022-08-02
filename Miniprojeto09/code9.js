0b2ea62d72af888b00d93d6d57c9b09110e6ab0d
unknown <tamaragleicee56@gmail.com>
2022-08-02 14:18:49 -0300

    Média de Nota Escolar

---
+++ Miniprojeto09/code9.js
@@ -0,0 +1,15 @@
+//Criar uma função que exibe a quantidade de *
+//que aquela linha possuí
+
+exibirAsteriscos(5);
+
+function exibirAsteriscos(linhas) {
+    let padrao = '';
+    for(let linha = 1; linha <= linhas; linha++){
+          padrao += '*';
+          console.log(padrao);
+    }
+}
+
+    
+    
