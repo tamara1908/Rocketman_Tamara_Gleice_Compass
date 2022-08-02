bfe7a3fb02b88a3d4837460cc5cad750031d70dd
unknown <tamaragleicee56@gmail.com>
2022-08-02 12:17:46 -0300

    Encontre o String

--- Miniprojeto06/code6.js
+++ Miniprojeto06/code6.js
@@ -1,6 +1,5 @@
 //Criar um método para ler propriedades de um objeto e
 //exibir somente as propriedades do tipo string que estão nesse objeto
-
 const filme = {
     titulo : 'Diário de uma paixão',
     ano : 2004,
